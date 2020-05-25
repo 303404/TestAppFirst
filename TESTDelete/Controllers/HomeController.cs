00b8bd3527b095cfa05b79bbe9713e29fec16120
unknown <murgodrudra@gmail.com>
2020-05-25 23:25:44 +0530

    initial commit

---
+++ TESTDelete/Controllers/HomeController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TESTDelete.Controllers
+{
+    [RouteArea("")]
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        [Route("Hello/{id :int}")]
+        public ActionResult GetName(int id)
+        {
+            return null;
+        }
+        public ActionResult GetNamee()
+        {
+            return View();
+        }
+        public ActionResult CheckCountValue(int i)
+        {
+            if (i < 20)
+            {
+                //business logic goes here  
+            }
+            else
+            {
+                throw (new Exception("Out of the Range"));
+            }
+
+            return View();
+        }
+    }
+}
---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Web.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TESTDelete.Controllers;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            System.Diagnostics.Debugger.Break();
+            int count = 1;
+            HomeController controller = new HomeController();
+            ViewResult result = controller.CheckCountValue(count) as ViewResult;
+            Assert.IsNotNull(result);
+        }
+    }
+}
